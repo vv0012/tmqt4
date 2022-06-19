70716dd28d50cb9a863aeb0603296d1bd145b642
saeedghotb <Saeed.Ghotb@zoodfood.com>
2022-06-19 12:18:27 +0430

    change topic

--- src/services/command.service.js
+++ src/services/command.service.js
@@ -18,7 +18,7 @@ function postCommand(req, res, next) {
         message: "invalid data",
       });
     }
-    mqtt().publish("m-topic", data.schedule, (result, err) => {
+    mqtt().publish("tenis1/command", data.schedule, (result, err) => {
       if (err) {
         return res.status(500).json({
           message: "error while sending message to device",
